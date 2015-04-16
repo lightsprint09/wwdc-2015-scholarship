e64fbfabdaa46a88d86c6cc44e96224da9414cb6
Lukas Schmidt <lukas.schmi@gmail.com>
2015-04-16 19:25:50 +0200

    update

---
+++ LukasSchmidt/controller/CinemaManagerViewController.swift
@@ -0,0 +1,53 @@
+//
+//  CinemaManagerViewController.swift
+//  LukasSchmidt
+//
+//  Created by Lukas Schmidt on 16.04.15.
+//  Copyright (c) 2015 Lukas Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class CinemaManagerViewController: UIViewController, UICollectionViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6;
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("movieCell", forIndexPath: indexPath) as! UICollectionViewCell
+        
+        return cell
+    }
+    
+    @IBAction func dismissController(sender: AnyObject) {
+        presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- LukasSchmidt/iban/UIImage+MonoImage.m
+++ LukasSchmidt/iban/UIImage+MonoImage.m
@@ -46,9 +46,6 @@
             imageScale = self.scale;
         UIGraphicsBeginImageContextWithOptions(self.size, NO, imageScale);
     }
-    else {
-        UIGraphicsBeginImageContext(self.size);
-    }
 	
     [self drawInRect:rect];
 	
