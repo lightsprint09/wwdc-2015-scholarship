7009bff5a8e0c3542a3076c3dd0288c78e1e6637
Lukas Schmidt <lukas.schmi@gmail.com>
2015-04-16 15:07:21 +0200

    structure

---
+++ LukasSchmidt/view/RoundedCornerImageView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedCornerImageView.swift
+//  LukasSchmidt
+//
+//  Created by Lukas Schmidt on 16.04.15.
+//  Copyright (c) 2015 Lukas Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class RoundedCornerImageView: UIImageView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
